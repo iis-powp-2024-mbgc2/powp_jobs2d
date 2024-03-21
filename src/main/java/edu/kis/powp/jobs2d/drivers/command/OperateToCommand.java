c4c6d33df96046eeac7aa7c90bc80719ee65efe6
MateuszMagd <240742@edu.p.lodz.pl>
2024-03-21 13:06:33 +0100

    Big fixes

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,11 +1,22 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
 
+    private Job2dDriver driver;
+
+    private void operateTo(Job2dDriver driver, int x, int y)
+    {
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute() {
-
+        this.driver.operateTo(x, y);
     }
 }
