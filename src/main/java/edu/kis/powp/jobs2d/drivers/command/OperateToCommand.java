6a3c237d65b1b71577f57a285b1718faabc25d6a
MateuszMagd <240742@edu.p.lodz.pl>
2024-03-21 12:44:43 +0100

    Create Command interfae and classes

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void setPosition(int i, int i1) {
+
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,22 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand implements DriverCommand{
+    private int x;
+    private int y;
+
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void setPosition(int i, int i1) {
+
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+}
