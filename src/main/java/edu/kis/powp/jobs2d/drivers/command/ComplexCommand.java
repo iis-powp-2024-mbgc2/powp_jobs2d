133858f7900051c31f0660a03a060f4ff40dec59
MateuszMagd <240742@edu.p.lodz.pl>
2024-03-21 13:21:30 +0100

    First look for ComplexCommand

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
@@ -1,4 +1,37 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
-public class ComplexCommand {
+import java.util.ArrayList;
+
+public class ComplexCommand implements DriverCommand{
+
+    private ArrayList<DriverCommand> commands;
+
+    public ComplexCommand()
+    {
+        this.commands = new ArrayList<DriverCommand>();
+    }
+
+
+    public void addOpearteTo(OperateToCommand com)
+    {
+        commands.add(com);
+    }
+
+    public void addSetPosition(OperateToCommand com)
+    {
+        commands.add(com);
+    }
+
+    public void addComplexCommand(ComplexCommand com)
+    {
+        commands.add(com);
+    }
+
+    @Override
+    public void execute() {
+        for(DriverCommand com : commands)
+        {
+            com.execute();
+        }
+    }
 }
