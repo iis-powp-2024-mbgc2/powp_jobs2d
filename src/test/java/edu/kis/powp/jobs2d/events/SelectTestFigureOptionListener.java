cea20d79126213be9122a32c5498ec37e8d35999
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-02 15:42:05 +0200

    change to if statement to switch and introduce constants for "clean code"

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -7,9 +7,10 @@ import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
-
 	private final DriverManager driverManager;
 	private final int figureTestNumber;
+	public static final int FIGURE_1 = 1;
+	public static final int FIGURE_2 = 2;
 
 	public SelectTestFigureOptionListener(DriverManager driverManager, int figureTestNumber) {
 		this.driverManager = driverManager;
@@ -18,12 +19,15 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (figureTestNumber == 1) {
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		} else if (figureTestNumber == 2) {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		} else {
-			System.out.println("Figure with that number doesn't exist");
+		switch (figureTestNumber) {
+			case FIGURE_1:
+				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+				break;
+			case FIGURE_2:
+				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+			default:
+				System.out.println("Figure with that number doesn't exist");
 		}
 	}
 }
