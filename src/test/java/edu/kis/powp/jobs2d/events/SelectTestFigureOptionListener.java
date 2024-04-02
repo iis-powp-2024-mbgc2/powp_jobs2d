91efd8d807952d8c7ec6e95fe300a1d518aa3cd9
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-02 15:04:59 +0200

    Add test to show figure from figureScript2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -8,14 +8,22 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+	private final DriverManager driverManager;
+	private final int figureTestNumber;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, int figureTestNumber) {
 		this.driverManager = driverManager;
+		this.figureTestNumber = figureTestNumber;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if (figureTestNumber == 1) {
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		} else if (figureTestNumber == 2) {
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		} else {
+			System.out.println("Figure with that number doesn't exist");
+		}
 	}
 }
