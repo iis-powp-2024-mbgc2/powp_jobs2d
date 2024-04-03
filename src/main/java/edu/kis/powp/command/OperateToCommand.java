8f86ef6beb65fbb351d0bcd7cede591064cca40e
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-03 13:06:00 +0200

    Implement SetPositionCommand and OperateToCommand classes

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,15 +1,19 @@
 package edu.kis.powp.command;
 
-public class OperateToCommand implements DriverCommand {
-    private int x;
-    private int y;
+import edu.kis.powp.jobs2d.Job2dDriver;
 
-    OperateToCommand(int x, int y) {
+public class OperateToCommand implements DriverCommand {
+    private final int x;
+    private final int y;
+    private final Job2dDriver driver;
+
+    OperateToCommand(int x, int y, Job2dDriver driver) {
         this.x = x;
         this.y = y;
+        this.driver = driver;
     }
 
     public void execute() {
-        System.out.println("execute() from OperateToCommand...");
+        driver.operateTo(x, y);
     }
 }
