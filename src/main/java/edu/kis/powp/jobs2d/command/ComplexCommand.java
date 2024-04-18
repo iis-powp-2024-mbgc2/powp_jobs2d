c8f6908ac8cf19ae728e66b0a77003eb67453795
Czapi12344 <236013@edu.p.lodz.pl>
2024-04-18 20:53:58 +0200

    Create ComplexCommand.java

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,25 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+	private final List<DriverCommand> commands;
+	public ComplexCommand(){
+		this.commands = new ArrayList<>();
+	}
+	public void addCommand(DriverCommand command){
+		commands.add(command);
+	}
+	public void removeCommand(DriverCommand command){
+		commands.remove(command);
+	}
+	@Override
+	public void execute() {
+		for (DriverCommand command : commands){
+			command.execute();
+		}
+	}
+
+
+}
