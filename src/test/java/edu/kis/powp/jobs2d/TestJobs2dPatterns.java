83f671a78d47c270592bfaf48824490d910642e1
Jan Kozłowski <jasiokzl@gmail.com>
2024-06-19 11:16:12 +0200

    Tests for new figures

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -26,9 +26,15 @@ public class TestJobs2dPatterns {
     private static void setupPresetTests(Application application) {
         SelectTestFigureOptionListener selectTestFigureOptionListener1 = new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 1);
         SelectTestFigureOptionListener selectTestFigureOptionListener2 = new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 2);
+        SelectTestFigureOptionListener selectTestFigureOptionListener3 = new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 3);
+        SelectTestFigureOptionListener selectTestFigureOptionListener4 = new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 4);
+        SelectTestFigureOptionListener selectTestFigureOptionListener5 = new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 5);
 
         application.addTest("Figure Joe 1", selectTestFigureOptionListener1);
         application.addTest("Figure Joe 2", selectTestFigureOptionListener2);
+        application.addTest("Square", selectTestFigureOptionListener3);
+        application.addTest("Rectangle", selectTestFigureOptionListener4);
+        application.addTest("Triangle", selectTestFigureOptionListener5);
     }
 
     /**
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,26 +4,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.command.ComplexCommandFactory;
+import edu.kis.powp.jobs2d.drivers.command.DriverCommand;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
-	private final int testNumber;
+    private DriverManager driverManager;
+    private final int testNumber;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager, int testNumber) {
-		this.driverManager = driverManager;
-		this.testNumber = testNumber;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager, int testNumber) {
+        this.driverManager = driverManager;
+        this.testNumber = testNumber;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(testNumber == 1){
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		}
-		if(testNumber == 2) {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		}
-
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (testNumber == 1) {
+            FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        }
+        if (testNumber == 2) {
+            FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+        }
+        if (testNumber == 3) {
+            DriverCommand command = ComplexCommandFactory.createSquare(-100, -100, 50, driverManager.getCurrentDriver());
+            command.execute();
+        }
+        if (testNumber == 4) {
+            DriverCommand command = ComplexCommandFactory.createRectangle(-100, -100, 50, 80, driverManager.getCurrentDriver());
+            command.execute();
+        }
+        if (testNumber == 5) {
+            DriverCommand command = ComplexCommandFactory.createTriangle(-100, -100, -200, -100, 50, -200, driverManager.getCurrentDriver());
+            command.execute();
+        }
+    }
 }
